3d1e8cd74e0c768765f662845d967d5e35fb5817
Richard Wilson <rcwilson88@gmail.com>
2019-11-06 11:31:28 -0500

    added basic rendering to Weact

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const appContainer = document.querySelector("app")
 const header = document.createElement("h2");
 header.innerHTML = "Hola Mundo, cometelo por su culo jiji";
-appContainer.append(header)
+appContainer.appendChild(header)
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-const appContainer = document.querySelector("app")
+const Weact = require("./libs/weact")
+
+const appContainer = document.querySelector(".app")
 const header = document.createElement("h2");
-header.innerHTML = "Hola Mundo, cometelo por su culo jiji";
-appContainer.append(header)
+const queenBee = document.createElement("img");
+const beeButton = document.createElement("button")
+queenBee.src = "./img/beyonce-2.jpg";
+queenBee.classList.add("queen")
+header.innerHTML = "Hola Mundo, vamos a prometir nuestrue vida a la Reiña Beyonce";
+header.classList.add("header")
+Weact.wendew(appContainer, header)
+Weact.wendew(appContainer, queenBee)
+Weact.wendew(appContainer, beeButton)
+
 
