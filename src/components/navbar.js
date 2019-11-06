e7f71dc6c28e1a113ae019951eb20bb6976b22fe
Derrick White <derrickpwhite1@gmail.com>
2019-11-06 15:27:08 -0500

    started rendering front-end components (i.e., buttons, nav bar, etc.)

---
+++ src/components/navbar.js
@@ -0,0 +1,7 @@
+const Weact = require('../libs/weact');
+
+const wavbar = function (attributes, content) {
+    return Weact.cweate("wavbar", attributes, content)
+    }
+
+module.exports = wavbar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-const Weact = require("./libs/weact")
+const Weact = require("./libs/weact");
+// const wavbar = require("./components/navbar");
 
-const appContainer = document.querySelector(".app")
-const header = document.createElement("h2");
+const appContainer = document.querySelector(".app");
+// const header = document.createElement("h2");
+const footer = document.createElement("footer")
 const queenBee = document.createElement("img");
 const beeButton = document.createElement("button")
-queenBee.src = "./img/beyonce-2.jpg";
-queenBee.classList.add("queen")
-header.innerHTML = "Hola Mundo, vamos a prometir nuestrue vida a la Reiña Beyonce";
+
+const header = Weact.cweate("h2",{ style: "color:blue", href:"www.google.com/beyonce"},"Hola Mundo, vamos a prometir nuestrue vida a la Reina Beyonce");
+const wavbar = Weact.cweate("nav", {style: "color: green"}, "Awo yo, we got a wav bar wow witches");
 header.classList.add("header")
-Weact.wendew(appContainer, header)
-Weact.wendew(appContainer, queenBee)
-Weact.wendew(appContainer, beeButton)
+footer.classList.add("footer")
+queenBee.src = "./img/hiclipart.com-id_qnvkp.png";
+queenBee.classList.add("queen")
+beeButton.textContent = "Hail to the Queen"
 
-
+Weact.wender(appContainer, header)
+Weact.wender(appContainer, wavbar)
+Weact.wender(appContainer, footer)
+Weact.wender(appContainer, queenBee)
+Weact.wender(appContainer, beeButton)
