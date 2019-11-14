da8b3b8e71b2285c76f5c5f4d6a7890040bf6730
Brad Wilson <brad.n.wilson@live.com>
2019-11-14 13:35:44 -0500

    Songform updates

--- src/components/forms/SongForm.js
+++ src/components/forms/SongForm.js
@@ -20,28 +20,12 @@ function ArtistDropdown(artists) {
   artists.artists.map(artist => {
     artistList.push(Weact.cweate("option", {
       class: "artist-option",
-      value: artist._id,
-      onselect: () => {AlbumDropdown}
+      value: artist._id
     },
     artist.name
     ))
   })
   return artistList;
-}
-
-async function getAlbums(artists) {
-  const albumRes = await fetch(API_URL + "albums", {
-    method: "GET",
-    mode: "cors",
-    cache: "no-cache",
-    headers: {
-      "Accept": "application/json"
-    }
-  })
-  const albums = await albumRes.json();
-  const songs = albums.songs;
-  console.log(albums, songs);
-  return albums;
 }
 
 async function handleSubmit(event) {
@@ -64,45 +48,66 @@ async function handleSubmit(event) {
   });
 }
 
+function createAlbumMap(artists) {
+  const artistMap = {}
+  artists.artists.map(artist => {
+    artistMap[artist._id] = artist.albums;
+  })
+  return artistMap;
+}
+
+async function renderAlbums(event) {
+  const artistId = event.target.value;
+  document.querySelector(".selection-albums").innerHTML = "";
+  const rawResponse = await fetch(API_URL + "artists/" + artistId, {
+    mode: "cors",
+    method: "GET",
+    headers: {"Accept": "application/json"}
+  })
+  const artists = await rawResponse.json()
+  const albumList = document.querySelector(".selection-albums")
+  artists.foundArtist.albums.map(album => {
+    const albumItem = Weact.cweate("option", {class: "selection-albums-item", value: album._id, onmouseover: album.year}, album.title)
+    albumList.append(albumItem);
+  })
+}
+
+async function renderSongs(event) {
+  const albumId = event.target.value;
+  document.querySelector(".selection-songs").innerHTML = "";
+  const rawResponse = await fetch(API_URL + "albums/" + albumId, {
+    mode: "cors",
+    method: "GET",
+    headers: {"Accept": "application/json"}
+  })
+  const albums = await rawResponse.json();
+  const songList = document.querySelector(".selection-albums");
+  console.log(albums);
+  albums.requestedAlbum.songs.map(song => {
+    const songItem = Weact.cweate("option", {class: "seleciton-songs-item", value: song._id}, song.title)
+    songList.append(songItem);
+  })
+}
 
 async function SongForm() {
   document.querySelector(".container").innerHTML = "";
   const res = await getArtists();
   const artistList = ArtistDropdown(res);
-  return Weact.cweate("form", { onsubmit: handleSubmit }, 
-    Weact.cweate("select", { class: "selection"}, artistList)
-  )
-
-  console.log(res)
-  
-
-  
-
-  return Weact.cweate("form", { onsubmit: handleSubmit }, [
-    Weact.cweate(
-      "input",
-      { class: "song-title", placeholder: "Say My Name", type: "text" },
-      ""
+  const albumMap = createAlbumMap(res)
+  return Weact.cweate("div", { class: "form-wrapper"}, [
+    Weact.cweate("form", { onsubmit: handleSubmit }, 
+      Weact.cweate("select", { class: "selection-artists", onchange: await renderAlbums}, artistList)
     ),
-    Weact.cweate(
-      "input",
-      {
-        class: "song-duration",
-        placeholder: "Song Length (mm:ss)",
-        type: "text"
-      },
-      ""
+    Weact.cweate("form", { class: "form" },
+      Weact.cweate("select", { class: "selection-albums", onchange: await renderSongs}, "")
     ),
+    Weact.cweate("form", { class: "form"}, 
+      Weact.cweate("select", { class: "selection-songs"}, "")
+    )
 
-    Weact.cweate(
-      "input",
-      { class: "song-link", onchange: "Link to Song", type: "text" },
-      ""
-    ),
-
-    Button({ type: "submit" }, "Submit")
-  ]);
-
+  ])
 }
+
+  
 
 module.exports = SongForm;
